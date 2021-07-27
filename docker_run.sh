ecc9342e09f67fdefa60ac993ea7b7e0d9d73048
BenniFFM <benjamin@it-nickel.com>
2021-07-27 17:11:41 +0200

    changed line to copy config.yml to correct path

--- docker_run.sh
+++ docker_run.sh
@@ -5,7 +5,7 @@ fi
 if [ ! -f /docker/km200mqtt/km200.env ]; then
   cp ./km200.env /docker/km200mqtt/km200.env
 fi
-if [ ! -f /docker/km200/data/config.yml ]; then
-  cp ./config.yml /docker/km200/data/config.yml
+if [ ! -f /docker/km200mqtt/data/config.yml ]; then
+  cp ./config.yml /docker/km200mqtt/data/config.yml
 fi
 docker run -d --name KM200_to_MQTT --env-file /docker/km200/km200.env -v /docker/km200mqtt/data:/data --restart always km200mqtt
